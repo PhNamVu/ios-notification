14bc567c48d61218d92f1898a103f085a1333554
Nam Vu <phnamvu@gmail.com>
2021-11-21 21:08:22 +0700

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Nam Vu on 21/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
